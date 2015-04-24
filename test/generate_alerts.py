cfa177fb6cdec8810f1a111b0eb8e76e789d3520
Shuo Yang <imsure95@gmail.com>
2015-04-24 10:52:49 -0700

    test for camera alert generation

---
+++ test/generate_alerts.py
@@ -0,0 +1,18 @@
+"""
+Test camera alert stream generation.
+"""
+
+#!/usr/bin/env python3
+
+import sys, os
+
+## The main function.
+def main():
+    alerts = GenerateCameraAlertStream()
+    for alert in alerts:
+        print( alert )
+
+if __name__ == '__main__':
+    sys.path.append( '../' )
+    from antares import GenerateCameraAlertStream
+    main()
