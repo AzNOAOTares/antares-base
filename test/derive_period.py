8fb396a370989b10ae79dd7b5c1af37e33012d55
Shuo Yang <imsure95@gmail.com>
2015-04-28 12:01:11 -0700

    show how to handle history of computed values

--- test/derive_period.py
+++ test/derive_period.py
@@ -14,16 +14,30 @@ def ComputeP():
 ## The main function.
 def main():
     alerts = GenerateCameraAlertStream( alert_num=1 )
-    for alert in alerts:
-        try:
-            alert.CA.P.value = 'abc'
-        except TypeError:
-            print( 'TypeError caught because we are assigning an incompatible type of value!' )
-        
-        alert.CA.P.value = ComputeP()
-        alert.CA.P.confidence = 1.0
-        alert.CA.P.annotation = 'Computed with high confidence'
-        print( alert )
+    alert = alerts[ 0 ]
+
+    try:
+        alert.CA.P.value = 'abc'
+    except TypeError:
+        print( 'TypeError caught because we are assigning an incompatible type of value!' )
+
+    ## Suppose we first compute P value with lower confidence.
+    alert.CA.P.value = ComputeP()
+    alert.CA.P.confidence = 0.4
+    alert.CA.P.annotation = 'Computed with low confidence'
+
+    ## Later we compute P value with higher confidence, this will
+    ## be the most-recently computed value for P. But the previous one
+    ## will still be kept with its timpstamp.
+    alert.CA.P.value = ComputeP()
+    alert.CA.P.confidence = 1.0
+    alert.CA.P.annotation = 'Computed with high confidence'
+
+    # This will show the most-recently computed value.
+    print( alert )
+
+    # This will print out all the computed values for P and their timestamps.
+    print( alert.CA.P.history )
 
 if __name__ == '__main__':
     sys.path.append( '../' )
