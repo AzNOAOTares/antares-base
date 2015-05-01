5e24c1bb4a55493a7e7baec7ab61122a7073bc0b
Shuo Yang <imsure95@gmail.com>
2015-04-30 17:54:52 -0700

    full example of diverting an alert

--- examples/divertAlert_api.py
+++ examples/divertAlert_api.py
@@ -11,15 +11,15 @@ def filterVPDF( alert, rarityThreshold=1E7 ):
 
     if rarityVPDF < rarityThreshold:
         annotation = 'Alert variability less than VPDF threshold (%.2f <  %.2f)' %(rarityVPDF, rarityThreshold)
-        #print( annotation )
-        #alert.divert(anotation)
+        print( alert.parent.ID, annotation )
+        alert.divert( annotation )
 
 if __name__ == '__main__':
     sys.path.append( '../' )
     from antares.helper import GenerateCameraAlertStream
     from antares.helper import ConstructAlertFromID
 
-    alert_ids = GenerateCameraAlertStream( alert_num=20 )
+    alert_ids = GenerateCameraAlertStream( alert_num=2 )
     for alert_id in alert_ids:
         alert = ConstructAlertFromID( alert_id, 'E' )
 
