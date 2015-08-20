fafb369d0a44086da51c1070cd73d8cf7f105e51
Shuo Yang <imsure95@gmail.com>
2015-08-20 11:33:24 -0700

    add scripts

--- scripts/dump_db.sh
+++ scripts/dump_db.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/local/mysql/bin/mysqldump -u root antares_demo Alert AlertReplica Attribute AttributeValue AstroObject Locus > LA_Alert_DB.sql
+/usr/local/mysql/bin/mysqldump -u root antares_demo Alert AlertReplica Attribute AttributeValue AstroObject Locus AlertStatus Combo InCombo PLV_SDSS > LA_Alert_DB.sql
